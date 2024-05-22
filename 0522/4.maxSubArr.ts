594bd3a0a9923314b6663640efd2943fd26b0559
yousan <mx364730@alibaba-inc.com>
2024-05-22 23:47:14 +0800

    0522

---
+++ 0522/4.maxSubArr.ts
@@ -0,0 +1,9 @@
+const maxSubArray = function (nums: number[]): number {
+  let pre = 0;
+  let maxAns = nums[0];
+  nums.forEach((x) => {
+    pre = Math.max(pre + x, x);
+    maxAns = Math.max(maxAns, pre);
+  });
+  return maxAns;
+};
