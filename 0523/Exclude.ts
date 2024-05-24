eada913a40db2bdd79bea75207a5bae89d650ec8
yousan <mx364730@alibaba-inc.com>
2024-05-24 23:34:27 +0800

    ts: narrow init

--- 0523/Exclude.ts
+++ 0523/Exclude.ts
@@ -1,1 +1,11 @@
 type MyExclude<T, K> = T extends K ? never : T;
+
+interface callFn {
+  (arg: string): number;
+}
+
+type newFn = new () => { some: string };
+
+const call: callFn = () => {
+  return 1;
+};
